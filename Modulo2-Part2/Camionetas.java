7847629b1c6f4971aec2c2eb71c8140d7af75dfc
Luis Arcia <luis.arcia@outlook.com>
2022-02-24 16:40:00 -0500

    Add files via upload

---
+++ Modulo2-Part2/Camionetas.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.weblarc.programacion2.homework2.modulo2;
+/**
+ *
+ * @author luisarcia
+ */
+public class Camionetas extends Vehiculo {}
